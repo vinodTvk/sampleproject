d43b252cbd229a7c4cb833f803305d4f5e30136c
root <root@ip-172-31-31-44.ap-southeast-1.compute.internal>
2017-06-22 07:09:12 -0400

    project is added

---
+++ src/main/java/sampleproject/App.java
@@ -0,0 +1,13 @@
+package sampleproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
